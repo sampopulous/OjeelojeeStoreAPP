d163bf214ac6170133b23c9995c60187f0d0169e
sampopulous <65770070+sampopulous@users.noreply.github.com>
2020-06-02 00:55:54 +0530

    storeAPP

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+	'ApiUrl':"https://ojeelojee.com/merchantapp/api",
+	'DialogDefaultTitle':"Ojeelojee Store APP",
+	'APIHasKey':"ijsdjf8239n28u23n3o99u2h72822",
+	'debug': false
+};
